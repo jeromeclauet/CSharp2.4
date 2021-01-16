bd6c2f5f50fffd8fa87f8157396350fb31b317f1
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 12:43:24 +0100

    Exercice 5

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choississez un mois de l'année");
+            string month = Console.ReadLine();
+            if ((month == "Mars") || (month == "Avril") || (month == "Mai"))
+            {
+                Console.WriteLine("La saison du mois saisi est le PRINTEMPS");
+            }
+            else if ((month == "Juin") || (month == "Juillet") || (month == "Août"))
+            {
+                Console.WriteLine("La saison du mois saisi est l'ETE");
+            }
+            else if ((month == "Septembre") || (month == "Octobre") || (month == "Novembre")) 
+            {
+                Console.WriteLine("La saison du mois saisi est l'AUTOMNE");
+            }
+            else if ((month == "Décembre") || (month == "Janvier") || (month == "Février"))
+            {
+                Console.WriteLine("La saison du mois saisi est l'HIVER");
+            }
+            else 
+            {
+                Console.WriteLine("Le mois est mal saisi ou mal orthographié");
+            }
+        }
+    }
+}
